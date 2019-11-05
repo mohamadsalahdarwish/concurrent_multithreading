9169ba162156ab8a38a0101b6d73decbad4b0c7f
mohammad_darwish <mohammad_darwish@siliconexpert.com>
2019-11-05 13:50:39 +0200

    code_for_session

---
+++ Java8ParallelStream/src/main/java/com/jcg/java/ParallelStreamDemo.java
@@ -0,0 +1,44 @@
+package com.jcg.java;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/*
+ * To enable 'parallelism', all you have to do is to create a parallel stream, 
+ * instead of a sequential stream. And to suprise you, this is really very
+ * easy.
+ * 
+ * Anytime you want to particular job using multiple threads in parallel cores,
+ * all you have to call the 'parallelStream()' method instead of the 'stream()'
+ * method.
+ */
+public class ParallelStreamDemo {
+
+	public static void main(String[] args) {
+
+		long t1, t2;
+		List<Employee> eList = new ArrayList<Employee>();
+		for(int i=0; i<100; i++) {
+			eList.add(new Employee("A", 20000));
+			eList.add(new Employee("B", 3000));
+			eList.add(new Employee("C", 15002));
+			eList.add(new Employee("D", 7856)); 
+			eList.add(new Employee("E", 200)); 
+			eList.add(new Employee("F", 50000));
+		}
+
+		/***** Here We Are Creating A 'Sequential Stream' & Displaying The Result *****/
+		t1 = System.currentTimeMillis();   
+		System.out.println("Sequential Stream Count?= " + eList.stream().filter(e -> e.getSalary() > 15000).count());
+
+		t2 = System.currentTimeMillis();
+		System.out.println("Sequential Stream Time Taken?= " + (t2-t1) + "\n");
+
+		/***** Here We Are Creating A 'Parallel Stream' & Displaying The Result *****/
+		t1 = System.currentTimeMillis();		
+		System.out.println("Parallel Stream Count?= " + eList.parallelStream().filter(e -> e.getSalary() > 15000).count());
+
+		t2 = System.currentTimeMillis();
+		System.out.println("Parallel Stream Time Taken?= " + (t2-t1));
+	}
+}
---
+++ generalcases/src/LockDemo.java
@@ -0,0 +1,57 @@
+
+
+import java.util.concurrent.locks.ReentrantLock;
+
+
+
+public class LockDemo {
+
+	private static int counter = 0;
+	private static java.util.concurrent.locks.Lock lock = new ReentrantLock();
+	
+	public static void increment(){
+		lock.lock();
+		counter++;
+		lock.unlock();
+	}
+	
+	public static void firstThread(){
+		for(int i=0;i<1000;i++){
+			increment();
+		}
+	}
+	
+	public static void secondThread(){
+		for(int i=0;i<1000;i++){
+			increment();
+		}
+	}
+	
+	public static void main(String[] args) {
+		
+		Thread t1 = new Thread(new Runnable() {
+			public void run() {
+				firstThread();
+			}
+		});
+		
+		Thread t2 = new Thread(new Runnable() {
+			public void run() {
+				firstThread();
+			}
+		});
+		
+		t1.start();
+		t2.start();
+		
+		try {
+			t1.join();
+			t2.join();
+		} catch (InterruptedException e) {
+			e.printStackTrace();
+		}
+		
+		System.out.println(counter);
+		
+	}
+}
